87bb0c0d33e89cae73b03c1b8a9be59bf0fb6bca
ccameron2 <74011941+ccameron2@users.noreply.github.com>
2024-06-08 22:16:56 +0100

    more attempts to build

--- assimp.lua
+++ assimp.lua
@@ -1,17 +1,43 @@
 project "assimp"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++17"
     staticruntime "on"
+
+    buildoptions("/MP")
 
     targetdir("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
     objdir("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
     defines {
+        "WIN32",
+        "_WINDOWS",
+        "OPENDDL_STATIC_LIBARY",
+        "ASSIMP_BUILD_NO_M3D_IMPORTER",
+        "ASSIMP_BUILD_NO_M3D_EXPORTER",
+        --"ASSIMP_BUILD_NO_GLTF2_IMPORTER",
+       --"ASSIMP_BUILD_NO_GLTF2_EXPORTER",
+        "ASSIMP_BUILD_NO_IFC_IMPORTER",
+        "WIN32_LEAN_AND_MEAN",
+        "UNICODE",
+        "_UNICODE",
+        "MINIZ_USE_UNALIGNED_LOADS_AND_STORES=0",
+        "ASSIMP_IMPORTER_GLTF_USE_OPEN3DGC=1",
+        "RAPIDJSON_HAS_STDSTRING=1",
+        "RAPIDJSON_NOMEMBERITERATORCLASS",
+        "ASSIMP_BUILD_NO_C4D_IMPORTER",
+        "RAPIDJSON_NOMEMBERITERATORCLASS",
+        "_SCL_SECURE_NO_WARNINGS",
+        "_CRT_SECURE_NO_WARNINGS",
+        "OPENDDLPARSER_BUILD",
+        "assimp_EXPORTS"
     }
   
     files {
-        "code/**",
-        "include/**",
+        "code/**.h",
+        "code/**.cpp",
+        "include/**.h",
+        "include/**.cpp",
     }
   
     includedirs {
@@ -20,7 +46,11 @@ project "assimp"
         "code",
         "contrib",
         "contrib/pugixml/src",
-        "contrib/zlib"
+        "contrib/zlib",
+        "contrib/unzip",
+        "contrib/rapidjson/include",
+        "contrib/openddlparser/include",
+        "contrib/utf8cpp/source",
     }
 
     filter "system:windows"
